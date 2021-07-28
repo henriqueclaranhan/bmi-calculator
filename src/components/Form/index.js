31ea60e9bace0871a0e3446627d9eef0f1b079bb
Henrique <henrique.claranhan@outlook.com>
2021-07-28 14:58:05 -0400

    add useState

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import { func } from 'prop-types';
+import React, { useState } from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
 import BmiResult from './BmiResult';
 
 export default function Form(){
+
+    const [height, setHeight] = useState(null);
+    const [weight, setWeight] = useState(null);
+    const [message, setMessage] = useState("Fill out the weight and height fields");
+    const [bmi, setBmi] = useState(null);
+    const [textButton, setTextButton] = useState("Calculate");
+
     return (
         <View>
            <View>
@@ -10,7 +18,7 @@ export default function Form(){
                <TextInput placeholder="e.g. 1.70" keyboardType="numeric" />
                <Text>Weight</Text>
                <TextInput placeholder="e.g. 80" keyboardType="numeric" />
-               <Button title="Calculate"/>
+               <Button title={textButton}/>
            </View>
            <BmiResult bmiResultMessage={message} bmiResult={bmi}/>
         </View>
