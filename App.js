e16247d1a390943bbee53a66bd24d719dd1348a9
Henrique <henrique.claranhan@outlook.com>
2021-07-29 01:49:08 -0400

    import fonts

--- App.js
+++ App.js
@@ -1,24 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
 import Title from './src/components/Title';
 import Form from './src/components/Form';
+import { useFonts } from 'expo-font';
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<StatusBar style="auto" />
-			<Title />
-			<Form />
-		</View>
-	);
+	let [fontsLoaded] = useFonts({
+		'Poppins-Bold': require('./assets/fonts/Poppins-Bold.ttf'),
+		'Poppins-ExtraLight': require('./assets/fonts/Poppins-ExtraLight.ttf'),
+		'Poppins-Light': require('./assets/fonts/Poppins-Light.ttf'),
+		'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
+		'Poppins-Medium': require('./assets/fonts/Poppins-Medium.ttf'),
+		'Poppins-Thin': require('./assets/fonts/Poppins-Thin.ttf')
+	  });
+	
+	  if (!fontsLoaded) {
+		return <AppLoading />;
+	  }
+	  
+	  else {
+		return (
+			<View style={styles.container}>
+				<StatusBar style="dark" />
+				<Title />
+				<Form />
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#f3f3f3',
-		alignItems: 'center',
-		justifyContent: 'center',
+		backgroundColor: '#f3f3f3'
 	},
 });
